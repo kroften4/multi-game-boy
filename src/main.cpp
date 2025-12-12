d7a2ec37a92b2047b8796990367c90d5492dc0ba
Stanislav <sskozhemyakin2006@gmail.com>
2025-12-12 12:32:14 +0300

    pinout

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,32 @@
+#include "sys/unistd.h"
 #include <Arduino.h>
+#include <TFT_eSPI.h>
 
-void setup() {}
-void loop() {}
+TFT_eSPI tft = TFT_eSPI();
+TFT_eSprite spr_dvdlogo = TFT_eSprite(&tft);
+
+void print_manifesto();
+
+void setup(void)
+{
+    tft.init();
+    tft.fillScreen(TFT_BLACK);
+    tft.setRotation(0);
+
+    // tft.setTextFont(1);
+    // tft.setTextSize(2);
+    // tft.setTextColor(TFT_WHITE, TFT_BLACK);
+    // tft.setTextDatum(CC_DATUM);
+    // tft.setTextWrap(true);
+}
+
+void loop()
+{
+    // tft.fillRect(0, 0, tft.width(), tft.height(), TFT_RED);
+    // sleep(5);
+    tft.fillRect(0, 0, tft.width(), tft.height(), TFT_GREEN);
+    sleep(5);
+    // tft.fillRect(0, 0, tft.width(), tft.height(), TFT_BLUE);
+    // sleep(5);
+}
+
